8922e126574e450500a14ab18baf7a2098cc74da
Leklo <leklo.sounds@gmail.com>
2025-02-18 00:37:08 +0100

    Quick clean then bedtime

--- player/movement/actions/surface-control/PM_SurfaceState.cs
+++ player/movement/actions/surface-control/PM_SurfaceState.cs
@@ -5,10 +5,9 @@ using Godot;
 public partial class PM_SurfaceState : Node
 {
     // [Export] public PM_Crouch CrouchHandler {get; private set;}
-    // [Export] public PM_Sprint SprintHandler {get; private set;}
     [Export] public PM_SurfaceStateData StateData {get; private set;}
     [Export] public PI_Sprint SprintInput {get; private set;}
-    public PM_SurfaceData CurrentData {get; private set;}
+    public PM_SurfaceData CurrentData {get; protected set;}
 
     public override void _Ready()
     {
