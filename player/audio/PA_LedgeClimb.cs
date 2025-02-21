df2b3882824429454c8607b59f25479516ad688a
Leklo <leklo.sounds@gmail.com>
2025-02-21 22:17:22 +0100

    PA_LedgeClimb typo ...

--- player/audio/PA_LedgeClimb.cs
+++ player/audio/PA_LedgeClimb.cs
@@ -1,7 +1,7 @@
 using Godot;
 
 [GlobalClass]
-public partial class PA_LedgeClimn : PA_LayeredSound
+public partial class PA_LedgeClimb : PA_LayeredSound
 {
     [Export] private PM_LedgeClimb _LedgeClimb;
     public override void _Ready()
