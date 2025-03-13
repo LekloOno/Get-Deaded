9f81440f5b0371926fa3ae1a80aab17846bdce60
Leklo <leklo.sounds@gmail.com>
2025-03-14 00:46:43 +0100

    WE_Death - revive input

--- world/events/WE_Death.cs
+++ world/events/WE_Death.cs
@@ -4,9 +4,42 @@ using Godot;
 public partial class WE_Death : Node
 {
     [Export] private PM_Controller _playerController;
+    [Export] private float _holdTime = 1.5f;
+    [Export] private float _releaseTime = .8f;
+    private float _chargeTracker = 0f;
+    private float _chargeTime = 0f;
+    private bool _reviveDown;
 
     public override void _Ready()
     {
-        _playerController.OnDie += (o, e) => _playerController.Revive();
+        SetProcessUnhandledInput(false);
+        SetPhysicsProcess(false);
+        _playerController.OnDie += (o, e) => SetProcessUnhandledInput(true);
+    }
+
+    public override void _UnhandledKeyInput(InputEvent @event)
+    {
+        if (@event.IsActionPressed("revive"))
+        {
+            SetPhysicsProcess(true);
+            _chargeTime = _holdTime;
+        }
+        else if (@event.IsActionReleased("revive"))
+            _chargeTime = -_releaseTime;
+    }
+
+    public override void _PhysicsProcess(double delta)
+    {
+        _chargeTracker += (float)delta/_chargeTime;
+        if (_chargeTracker >= 1f)
+        {
+            SetProcessUnhandledInput(false);
+            _playerController.Revive();
+            _chargeTracker = 0f;
+        }
+        else if (_chargeTracker > 0f)
+            return;
+
+        SetPhysicsProcess(false);
     }
 }
