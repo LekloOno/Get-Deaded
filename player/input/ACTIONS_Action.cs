713aa0a8e6db01b33e2392287419036324c7b93a
Leklo <leklo.sounds@gmail.com>
2025-03-17 14:29:24 +0100

    Input overhaul - quick reorganization

--- player/input/ACTIONS_Action.cs
+++ player/input/ACTIONS_Action.cs
@@ -3,5 +3,5 @@ using Godot;
 public abstract class ACTIONS_Action(StringName value)
 {
     public StringName Value { get; } = value;
-    public static implicit operator StringName(ACTIONS_Action direction) => direction.Value;
+    public static implicit operator StringName(ACTIONS_Action action) => action.Value;
 }
