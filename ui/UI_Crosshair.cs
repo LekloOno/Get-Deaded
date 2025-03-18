d9ddfc3dc8948930c7187321534a019f4db282d4
Leklo <leklo.sounds@gmail.com>
2025-03-18 19:51:41 +0100

    UI_Crosshair update - finer position smoothing

--- ui/UI_Crosshair.cs
+++ ui/UI_Crosshair.cs
@@ -1,3 +1,4 @@
+using System;
 using Godot;
 
 [GlobalClass]
@@ -6,12 +7,15 @@ public partial class UI_Crosshair : Control
     [Export] private Node3D _sightPosition;
     [Export] private Camera3D _camera;
     [Export] private Control image;
-    [Export] float _maxRange = 30;
-    [Export] float _minRange = 2;
-    [Export] float _resetSpeed = 30;
+    [Export] float _maxRange = 20;
+    [Export] float _minRange = 1.5f;
+    [Export] float _smoothdistanceThreshold = 5f;
+    [Export] float _smoothPositionThreshold = 100f;
+    [Export] float _smoothSpeed = 10f;
 
     private Vector2 _imageOffset;
-    private Vector2 _initPosition;
+    private float _distance;
+    private float _targetDistance;
     private Vector2 _targetPosition;
 
     public override void _Ready()
@@ -19,41 +23,49 @@ public partial class UI_Crosshair : Control
         _imageOffset = image.Size/2;
 
         Vector2 viewportSize = GetViewport().GetVisibleRect().Size;
-        _initPosition = viewportSize/2 - _imageOffset;
     }
 
     public override void _Process(double delta)
     {
         var spaceState = _camera.GetWorld3D().DirectSpaceState;
 
+        Vector3 origin = _sightPosition.GlobalPosition;
+        Vector3 direction = - _sightPosition.GlobalBasis.Z;
+
         PhysicsRayQueryParameters3D query = PhysicsRayQueryParameters3D.Create(
-            _sightPosition.GlobalPosition, 
-            _sightPosition.GlobalPosition - _sightPosition.GlobalBasis.Z * _maxRange
+            origin, 
+            origin + direction * _maxRange
         );
+
+        query.CollideWithAreas = true;
+        query.CollisionMask = 0b11;
 
         var result = spaceState.IntersectRay(query);
 
         if (result.Count > 0)
         {
             Vector3 hitPosition = (Vector3)result["position"];
-            float distance = _sightPosition.GlobalPosition.DistanceTo(hitPosition);
-            
-            if (distance > _minRange)
-            {
-                _targetPosition = _camera.UnprojectPosition(hitPosition)  - _imageOffset;
-            }
-            else
-            {
-                Vector3 minRangePosition = _sightPosition.GlobalPosition - _sightPosition.GlobalBasis.Z * _minRange;
-                _targetPosition = _camera.UnprojectPosition(minRangePosition) - _imageOffset;
-                //image.Position = Vector2.Zero;
-            }
+            _targetDistance = _sightPosition.GlobalPosition.DistanceTo(hitPosition);
+            _targetDistance = Mathf.Clamp(_targetDistance, _minRange, _maxRange);
         }
         else
         {
-            _targetPosition = _initPosition;
+            _targetDistance = _maxRange;
         }
 
-        image.Position = image.Position.Lerp(_targetPosition, (float)delta * _resetSpeed);
+        _targetPosition = _camera.UnprojectPosition(origin + direction * _targetDistance) - _imageOffset;
+
+        if (TriggerSmoothDistance(delta) && TriggerSmoothPosition(delta))
+        {
+            _distance = Mathf.Lerp(_distance, _targetDistance, _smoothSpeed * (float) delta);
+            _targetPosition = _camera.UnprojectPosition(origin + direction * _distance) - _imageOffset;
+        }
+        else
+            _distance = _targetDistance;
+
+        image.Position = _targetPosition;
     }
+
+    private bool TriggerSmoothDistance(double delta) => Math.Abs(_distance - _targetDistance) > _smoothdistanceThreshold * (float) delta;
+    private bool TriggerSmoothPosition(double delta) => _targetPosition.DistanceTo(image.Position) > _smoothPositionThreshold * (float) delta;
 }
