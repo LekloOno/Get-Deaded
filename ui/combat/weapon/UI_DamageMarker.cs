e7ae6edac2fb80e2a706830ce1a7c9efb96d4704
Leklo <leklo.sounds@gmail.com>
2025-03-19 10:51:37 +0100

    UI_HitMarker - Reorganized and cleaned code with a dedicated UI_DamageMarker

---
+++ ui/combat/weapon/UI_DamageMarker.cs
@@ -0,0 +1,99 @@
+using System.Linq;
+using Godot;
+using Godot.Collections;
+
+[GlobalClass]
+public partial class UI_DamageMarker : Control
+{
+    [Export] private Array<Panel> _markerSticks;
+    [Export] private Color _headShotColor;
+    [Export] private Color _normalColor;
+    [Export] private float _fadeTime = .18f;
+    [Export] private float _expandTime = .1f;
+    [Export] private float _baseStartOffset = 12f;
+    [Export] private float _basePeakOffset = 15f;
+    [Export] private Vector2 _baseSize = new(16f, 2f);
+
+    // Higher values means higher damages are required to get the maximum size.
+    // It is the dispersion of the tanh() function used to convert the damage to a size.
+    [Export] private float _sizeDispersion = 300f;
+    // The maximum size coeficient, x times bigger than _baseSize.
+    [Export] private float _sizeMaxCoef = 5f; // The final coeficient (-1) could be computed on ready
+    
+    // Same as for the dispersion but for the peak offset
+    [Export] private float _offsetDispersion = 20f;
+    [Export] private float _offsetMaxCoef = 2.5f;
+
+    private StyleBoxFlat _hitStyle;
+    private Tween opacityTween;
+    private Tween offsetTween;
+    private Tween sizeTween;
+
+    private Vector2 _sticksSize;
+    public Vector2 SticksSize
+    {
+        get => _sticksSize;
+        set {
+            Vector2 scale = value;
+            Vector2 position = _markerSticks.ElementAt(0).Position;
+            position.Y = -scale.Y/2;
+            foreach (Panel stick in _markerSticks)
+            {
+                stick.Size = scale;
+                stick.Position = position;
+            }
+            
+            _sticksSize = value;
+        }
+    }
+
+    private float _offset;
+    public float Offset
+    {
+        get => _offset;
+        set {
+            Vector2 position = _markerSticks.ElementAt(0).Position;
+            position.X = value;
+            foreach (Panel stick in _markerSticks)
+                stick.Position = position;
+            
+            _offset = value;
+        }
+    }
+
+    public override void _Ready()
+    {
+        _hitStyle = (StyleBoxFlat) _markerSticks.ElementAt(0).GetThemeStylebox("panel");
+        Color mod = Modulate;
+        mod.A = 0f;
+        Modulate = mod;
+        Offset = _baseStartOffset;
+    }
+
+    public void StartAnim(ShotHitEventArgs e)
+    {
+        Color mod = Modulate;
+        mod.A = 1f;
+        Modulate = mod;
+
+        opacityTween?.Kill();
+        offsetTween?.Kill();
+
+        float sizeRatio = Mathf.Tanh(e.Damage/_sizeDispersion)*(_sizeMaxCoef - 1f) + 1f;
+        SticksSize = _baseSize * sizeRatio;
+
+        if (e.HurtBox.BodyPart == GC_BodyPart.Head)
+            _hitStyle.BgColor = _headShotColor;
+        else
+            _hitStyle.BgColor = _normalColor;
+        
+        opacityTween = CreateTween();
+        opacityTween.TweenProperty(this, "modulate:a", 0.0f, _fadeTime);
+
+        offsetTween = CreateTween();
+        float scaledDamage = Mathf.Tanh(e.Damage/_offsetDispersion) * (_offsetMaxCoef-1f) + 1;
+
+        offsetTween.TweenProperty(this, "Offset", _basePeakOffset * scaledDamage, _expandTime).SetEase(Tween.EaseType.Out).SetTrans(Tween.TransitionType.Sine);
+        offsetTween.TweenProperty(this, "Offset", _baseStartOffset, _expandTime);
+    }
+}
--- ui/combat/weapon/UI_HitMarker.cs
+++ ui/combat/weapon/UI_HitMarker.cs
@@ -5,63 +5,13 @@ using Godot.Collections;
 [GlobalClass]
 public partial class UI_HitMarker : Control
 {
-    [Export] private Array<Panel> _markerSticks;
-    [Export] private UI_KillMarker _killMarker;
-    [Export] private Control _damageMarker;
     [Export] private PW_WeaponsHandler _weaponsHandler;
-    [Export] private Color _headShotColor;
-    [Export] private Color _normalColor;
-    [Export] private float _fadeTime;
-    [Export] private float _expandTime;
-    [Export] private float _baseStartOffset = 12f;
-    [Export] private float _basePeakOffset = 15f;
-    [Export] private Vector2 _baseSize = new Vector2(18, 4);
-
-    private Vector2 _sticksSize;
-    public Vector2 SticksSize
-    {
-        get => _sticksSize;
-        set {
-            Vector2 scale = value;
-            Vector2 position = _markerSticks.ElementAt(0).Position;
-            position.Y = -scale.Y/2;
-            foreach (Panel stick in _markerSticks)
-            {
-                stick.Size = scale;
-                stick.Position = position;
-            }
-            
-            _sticksSize = value;
-        }
-    }
-
-    private float _offset;
-    public float Offset
-    {
-        get => _offset;
-        set {
-            Vector2 position = _markerSticks.ElementAt(0).Position;
-            position.X = value;
-            foreach (Panel stick in _markerSticks)
-                stick.Position = position;
-            
-            _offset = value;
-        }
-    }
-
-    private StyleBoxFlat _hitStyle;
-    private Tween opacityTween;
-    private Tween offsetTween;
-    private Tween sizeTween;
+    [Export] private UI_KillMarker _killMarker;
+    [Export] private UI_DamageMarker _damageMarker;
 
     public override void _Ready()
     {
         _weaponsHandler.Hit += HandleHit;
-        _hitStyle = (StyleBoxFlat) _markerSticks.ElementAt(0).GetThemeStylebox("panel");
-        Color mod = _damageMarker.Modulate;
-        mod.A = 0f;
-        _damageMarker.Modulate = mod;
-        Offset = _baseStartOffset;
     }
 
     public void HandleHit(object sender, ShotHitEventArgs e)
@@ -75,29 +25,6 @@ public partial class UI_HitMarker : Control
             return;
         }
 
-        Color mod = _damageMarker.Modulate;
-        mod.A = 1f;
-        _damageMarker.Modulate = mod;
-
-        opacityTween?.Kill();
-        offsetTween?.Kill();
-
-        float sizeRatio = (Mathf.Tanh(e.Damage/300f)*2f + 1f)/2f;
-        Vector2 targetSize = new((int)(_baseSize.X * sizeRatio), (int)(_baseSize.Y * sizeRatio));
-        SticksSize = targetSize * 2f;
-
-        if (e.HurtBox.BodyPart == GC_BodyPart.Head)
-            _hitStyle.BgColor = _headShotColor;
-        else
-            _hitStyle.BgColor = _normalColor;
-        
-        opacityTween = CreateTween();
-        opacityTween.TweenProperty(_damageMarker, "modulate:a", 0.0f, _fadeTime);
-
-        offsetTween = CreateTween();
-        float scaledDamage = Mathf.Tanh(e.Damage/20f) * 1.5f + 1;
-
-        offsetTween.TweenProperty(this, "Offset", _basePeakOffset * scaledDamage, _expandTime).SetEase(Tween.EaseType.Out).SetTrans(Tween.TransitionType.Sine);
-        offsetTween.TweenProperty(this, "Offset", _baseStartOffset, _expandTime);
+        _damageMarker.StartAnim(e);
     }
 }
