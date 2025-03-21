18641fcd3122b7e9f29034ecf7063e375fca13e3
Leklo <leklo.sounds@gmail.com>
2025-03-21 16:54:32 +0100

    PW_Fire - Spread impl

--- player/combat/weapons/core/fire/PW_Fire.cs
+++ player/combat/weapons/core/fire/PW_Fire.cs
@@ -9,11 +9,11 @@ public abstract partial class PW_Fire : Resource
 {
     private const double _bufferMargin = 0.008;
     [Export] private Array<PW_Shot> _shots;
-    [Export] protected float _spread;
+    [Export] protected float _spread = 0;
     [Export] protected ulong _fireRate;
     [Export] protected PW_Recoil _recoil;
-    public PHX_AdditiveModifiers SpreadMultiplier {get; private set;}
-    public PHX_AdditiveModifiers RecoilMultiplier {get; private set;}
+    public PHX_AdditiveModifiers SpreadMultiplier {get; private set;} = new();
+    public PHX_AdditiveModifiers RecoilMultiplier {get; private set;} = new();
     public EventHandler<ShotHitEventArgs> Hit;
     protected Node3D _sight;
     protected PC_DirectCamera _camera;
@@ -23,6 +23,7 @@ public abstract partial class PW_Fire : Resource
     private SceneTreeTimer _bufferTimer;
     private bool _pressBuffered = false;
     private bool _releaseBuffered = false;
+    private static Random _random = new();
 
     public void Initialize(PC_DirectCamera camera, Node3D sight, Node3D _barel)
     {
@@ -51,6 +52,17 @@ public abstract partial class PW_Fire : Resource
         _lastShot = Time.GetTicksMsec();
 
         SightTo(out Vector3 origin, out Vector3 direction);
+
+        float spread = Mathf.Max(_spread * SpreadMultiplier.Result(), 0f);
+        if (spread != 0)
+        {
+            Vector3 perp = _sight.GlobalBasis.X;
+            float theta = (float)(_random.NextDouble() * 2.0 * Mathf.Pi);
+            Vector3 rotationAxis = perp.Rotated(direction.Normalized(), theta).Normalized();
+
+            float spreadAngle = Mathf.DegToRad((float)_random.NextDouble()*spread);
+            direction = direction.Rotated(rotationAxis, spreadAngle);
+        }
         
         foreach (PW_Shot shot in _shots)
             shot.Shoot(_sight, origin, direction);
