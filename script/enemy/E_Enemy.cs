ba15509658b621f4d858aff98124ab0c31d00e37
Leklo <leklo.sounds@gmail.com>
2025-11-18 21:58:14 +0100

    Beginned enemy doc

---
+++ script/enemy/E_Enemy.cs
@@ -0,0 +1,133 @@
+using System;
+using Godot;
+
+public partial class E_Enemy : CharacterBody3D
+{
+    [Export] private GC_HealthManager _healthManager;
+    [Export] private float _hideDelay;
+    [Export] private MeshInstance3D _surfaceMesh;
+    [Export] private MeshInstance3D _jointMesh;
+    [Export] private GL_Dropper _lootDropper;
+    [Export] private PCT_Undirect _traumaCauser;
+    [Export] private float _drag = 10f;
+    [Export] private Color _hitColor = new(1f, 1f, 1f, 1f);
+    [Export] private float _hitTime = 0.15f;
+    private ShaderMaterial _surfaceMeshMaterial;
+    private ShaderMaterial _jointMeshMaterial;
+    private SceneTreeTimer _hideTimer;
+    public HealthEventHandler OnDie { get => _healthManager.TopHealthLayer.OnDie; set => _healthManager.TopHealthLayer.OnDie = value;}
+    public HealthEventHandler<DamageEventArgs> OnDamage { get => _healthManager.TopHealthLayer.OnDamage; set => _healthManager.TopHealthLayer.OnDamage = value;}
+
+    private BaseMaterial3D.ShadingModeEnum _initialJointShadingMode;
+    private Color _initialColor;
+    private Color _initialJointColor;
+    private SceneTreeTimer _hitResetTimer;
+
+    public float Alpha
+    {
+        get => ((Color) _surfaceMeshMaterial.GetShaderParameter("albedo")).A;
+        set
+        {
+            Color color = (Color) _surfaceMeshMaterial.GetShaderParameter("albedo");
+            color.A = value;
+            _surfaceMeshMaterial.SetShaderParameter("albedo", color);
+            Color jointColor = (Color) _jointMeshMaterial.GetShaderParameter("albedo");
+            jointColor.A = value;
+            _jointMeshMaterial.SetShaderParameter("albedo", jointColor);
+        }
+    }
+
+    public override void _Ready()
+    {
+        if(_surfaceMesh?.Mesh.SurfaceGetMaterial(0) is ShaderMaterial surfaceMat)
+        {
+            _surfaceMeshMaterial = surfaceMat;
+            _initialColor = (Color) surfaceMat.GetShaderParameter("albedo");
+        }
+
+        if(_jointMesh?.Mesh.SurfaceGetMaterial(0) is ShaderMaterial jointMat)
+        {
+            _jointMeshMaterial = jointMat;
+            _initialJointColor = (Color) jointMat.GetShaderParameter("albedo");
+        }
+
+        Enable();
+        
+
+        OnDie += PlayDeath;
+        OnDamage += PlayHit;
+    }
+
+    private void PlayHit(GC_Health senderLayer, DamageEventArgs e)
+    {
+        _surfaceMeshMaterial.SetShaderParameter("albedo", _hitColor);
+
+        if (_hitResetTimer != null)
+            _hitResetTimer.Timeout -= ResetHitMaterial;
+
+        _hitResetTimer = GetTree().CreateTimer(_hitTime);
+        _hitResetTimer.Timeout += ResetHitMaterial;
+    }
+
+    private void ResetHitMaterial()
+    {
+        _surfaceMeshMaterial.SetShaderParameter("albedo", _initialColor);
+        _jointMeshMaterial.SetShaderParameter("albedo", _initialJointColor);
+    }
+
+    public void PlayDeath(GC_Health health)
+    {
+        _lootDropper.Drop();
+        _traumaCauser.CauseTrauma();
+        Disable();
+    }
+
+    public void Disable()
+    {
+        CollisionLayer = 0;
+        _healthManager.DisableHurt();
+
+        //_hideTimer = GetTree().CreateTimer(_hideDelay);
+        //_hideTimer.Timeout += Hide;
+        
+        HideMesh();
+    }
+
+    public async void HideMesh()
+    {
+        Tween opacityTween = CreateTween();
+        opacityTween.TweenProperty(this, "Alpha", 0f, _hideDelay);
+
+        await ToSignal(opacityTween, "finished");
+        
+        Hide();
+        
+        SetPhysicsProcess(false);
+    }
+
+    public void Enable()
+    {
+        CollisionLayer = CONF_Collision.Layers.EnvironmentEntity;
+        Tween surfaceTween = CreateTween();
+        surfaceTween.TweenProperty(this, "Alpha", 1f, 0.2f);
+
+        Show();
+        SetPhysicsProcess(true);
+        _healthManager.EnableHurt(CONF_Collision.Layers.EnnemiesHurtBox);
+    
+        _healthManager.Init(true);
+    }
+
+    public override void _PhysicsProcess(double delta)
+    {
+        Vector3 velocity = Velocity;
+        if (!IsOnFloor())
+            velocity += GetGravity() * (float) delta;
+        else
+            velocity -= velocity * (float) delta * _drag;
+        
+        Velocity = velocity;
+        
+        MoveAndSlide();
+    }
+}
---
+++ script/enemy/E_EnemySpawner.cs
@@ -0,0 +1,27 @@
+using Godot;
+
+[GlobalClass]
+public partial class E_EnemySpawner : Node3D
+{
+    [Export] private E_Enemy _ennemy;
+    [Export] private float _respawnDelay;
+    private SceneTreeTimer _respawnTimer;
+    private SceneTreeTimer _hideTimer;
+
+    public override void _Ready()
+    {
+        _ennemy.OnDie += Die;
+    }
+
+    public void Die(GC_Health senderLayer)
+    {
+        _respawnTimer = GetTree().CreateTimer(_respawnDelay);
+        _respawnTimer.Timeout += Respawn;
+    }
+
+    public void Respawn()
+    {
+        _ennemy.GlobalPosition = GlobalPosition;
+        _ennemy.Enable();
+    }
+}
