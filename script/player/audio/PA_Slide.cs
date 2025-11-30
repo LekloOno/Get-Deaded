76887208e7de50d3926156a6fade3b0dfd188971
Leklo <leklo.sounds@gmail.com>
2025-11-30 20:09:51 +0100

    Some mixing

--- script/player/audio/PA_Slide.cs
+++ script/player/audio/PA_Slide.cs
@@ -8,10 +8,7 @@ using Godot.Collections;
 public partial class PA_Slide : Node3D
 {
     [Export] private PA_LayeredSound _slideIn;
-    [Export] private AudioStreamPlayer2D _slideHold;
-    [Export] private float _holdVolume;
-    [Export] private float _holdFadeInTime;
-    [Export] private float _holdFadeOutTime;
+    [Export] private PA_Looper _slideHold;
 
     [ExportCategory("Setup")]
     [Export] private PM_Slide _slide;
@@ -27,8 +24,8 @@ public partial class PA_Slide : Node3D
         _slideHold.VolumeDb = -80;
 
         _slide.OnStart += StartPlaySound;
-        _slide.OnStop += (o, e) => StopHold();
-        _slide.OnSlowStop += (o, e) => StopHold();
+        _slide.OnStop += (o, e) => _slideHold.StopLoop();
+        _slide.OnSlowStop += (o, e) => _slideHold.StopLoop();
 
         _groundState.OnLeaving += StopPlaySound;
         _groundState.OnLanding += TryLandStartHold;
@@ -40,60 +37,19 @@ public partial class PA_Slide : Node3D
         if(_groundState.IsGrounded())
         {
             _slideIn.PlayLayers();
-            StartHold();
+            _slideHold.StartLoop();
         }
     }
 
     private void StopPlaySound(object sender, EventArgs e)
     {
         _slideIn.StopLayers();
-        StopHold();
+        _slideHold.StopLoop();
     }
 
     private void TryLandStartHold(object sender, EventArgs e)
     {
         if (_slide.IsActive)
-            StartHold();
+            _slideHold.StartLoop();
     }
-
-    private void InitFade()
-    {
-        _fadeStart = Time.GetTicksMsec();
-        _startVolume = _slideHold.VolumeDb;
-        SetPhysicsProcess(true);
-    }
-
-    private void StartHold()
-    {
-        InitFade();
-        OnUpdate += FadeIn;
-    }
-
-    private void StopHold()
-    {
-        InitFade();
-        OnUpdate += FadeOut;
-    }
-
-
-    private void Fade(EventHandler handler, float currentFadeTime, float currentTargetVolume)
-    {
-        float elapsed = (Time.GetTicksMsec() - _fadeStart)/1000f;
-
-        if (elapsed >= currentFadeTime)
-        {
-            _slideHold.VolumeDb = currentTargetVolume;
-            OnUpdate -= handler;
-            SetPhysicsProcess(false);
-            return;
-        }
-
-        float elapsedScaled = elapsed/currentFadeTime;
-        float lerped = MATH_Sound.LerpDB(_startVolume, currentTargetVolume, elapsedScaled);
-        
-        _slideHold.VolumeDb = lerped;
-    }
-
-    private void FadeOut(object sender, EventArgs e) => Fade(FadeOut, _holdFadeOutTime, -80);
-    private void FadeIn(object sender, EventArgs e) => Fade(FadeIn, _holdFadeInTime, _holdVolume);
 }
