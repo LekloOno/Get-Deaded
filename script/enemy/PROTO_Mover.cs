bb780ec5998bdce68d37fa66383c1692450dc2e5
Leklo <leklo.sounds@gmail.com>
2025-12-19 00:20:38 +0100

    PROTO_Mover - add speed variation

--- script/enemy/PROTO_Mover.cs
+++ script/enemy/PROTO_Mover.cs
@@ -9,9 +9,11 @@ public partial class PROTO_Mover : Node
 
     private Vector3 _wishDir = Vector3.Zero;
     public Vector3 WishDir {get => _wishDir;}
-    
     private Timer _straffeTimer;
+    private Timer _speedTimer;
     private Random _rng = new Random();
+    private float _speed = 1f;
+    private bool _walking;
 
     public override void _Ready()
     {
@@ -20,6 +22,11 @@ public partial class PROTO_Mover : Node
         _straffeTimer.Timeout += ChangeStraffeDir;
         AddChild(_straffeTimer);
         StartStraffeTimer();
+
+        _speedTimer = new();
+        _speedTimer.Timeout += ChangeSpeed;
+        AddChild(_speedTimer);
+        StartSpeedTimer();
     }
 
     public void Rotate(Node3D self)
@@ -45,12 +52,39 @@ public partial class PROTO_Mover : Node
         float nextStaffe = Mathf.Lerp(_data.MinStraffe, _data.MaxStraffe, seed); 
         _straffeTimer.Start(nextStaffe);
     }
+
+    public void ChangeSpeed()
+    {
+        float seed = _rng.NextSingle();
+        _speed = PickSpeed(seed);
+        StartSpeedTimer();
+    }
+
+    public float PickSpeed(float seed)
+    {
+        if (seed < _data.PropWalk)
+            return _data.WalkSpeed;
+        
+        seed -= _data.PropWalk;
+        
+        if (seed < _data.PropRun)
+            return _data.RunSpeed;
+
+        return _data.SprintSpeed;
+    }
+
+    public void StartSpeedTimer()
+    {
+        float seed = _rng.NextSingle();
+        float nextChange = Mathf.Lerp(_data.MinHold, _data.MaxHold, seed);
+        _speedTimer.Start(nextChange);
+    }
     
 
     public Vector3 GetAcceleration(Node3D self, Vector3 velocity, double delta)
     {
         Vector3 globalDir = self.GlobalBasis * _wishDir;
-        Vector3 accel = PHX_MovementPhysics.Acceleration(_data.Speed, _data.Acceleration, velocity, globalDir.Normalized(), (float)delta);
+        Vector3 accel = PHX_MovementPhysics.Acceleration(_speed, _data.Acceleration, velocity, globalDir.Normalized(), (float)delta);
         return accel;
     }
 }
--- script/enemy/PROTO_MoverData.cs
+++ script/enemy/PROTO_MoverData.cs
@@ -3,8 +3,25 @@ using Godot;
 [GlobalClass]
 public partial class PROTO_MoverData : Resource
 {
-    [Export] public float Speed {get; private set;}
-    [Export] public float Acceleration {get; private set;}
-    [Export] public float MinStraffe {get; private set;}
-    [Export] public float MaxStraffe {get; private set;}
+    [ExportCategory("Straffe")]
+    [Export] public float MinStraffe {get; private set;} = 0.06f;
+    [Export] public float MaxStraffe {get; private set;} = 1.5f;
+
+    [ExportCategory("Base Speed")]
+    [Export] public float Acceleration {get; private set;} = 20f;
+    [Export] public float SprintSpeed {get; private set;} = 4.2f;
+    
+    [ExportCategory("Speed variations")]
+    [Export] public float RunSpeed {get; private set;} = 3.3f;
+    [Export] public float WalkSpeed {get; private set;} = 2.5f;
+    [Export] public float WalkWeight {get; private set;} = 0.15f;
+    [Export] public float RunWeight {get; private set;} = 0.35f;
+    [Export] public float SprintWeight {get; private set;} = 0.50f;
+    [Export] public float MinHold {get; private set;} = 0.1f;
+    [Export] public float MaxHold {get; private set;} = 3f;
+
+    public float TotalWeight => WalkWeight + RunWeight + SprintWeight;
+    public float PropWalk => WalkWeight / TotalWeight;
+    public float PropRun => RunWeight / TotalWeight;
+    public float PropSprint => SprintWeight / TotalWeight;
 }
