bdcd9a1e8de153e687599e3a88ec4e0c9046e95b
Leklo <leklo.sounds@gmail.com>
2025-12-29 15:13:39 +0100

    PWR_ConstantRecoil - added continuous recoil option, + WIP lightning gun

--- script/player/combat/weapons/recoil/PWR_ConstantRecoil.cs
+++ script/player/combat/weapons/recoil/PWR_ConstantRecoil.cs
@@ -4,18 +4,38 @@ using Godot;
 [GlobalClass]
 public partial class PWR_ConstantRecoil : PW_Recoil
 {
-    [Export] private bool _autoReset;   // Automatically resets recoil when it's completed. Otherwise, the Fire is responsible for calling Reset().
-    [Export] public Vector2 _angle;     // In degrees
-    [Export] private float _time;       // Time to reach the recoil angle, in seconds
-    [Export] private float _resetTime;  // Time to reset the recoil, in seconds
+    /// <summary>
+    /// Automatically resets recoil when it's completed. Otherwise, the Fire is responsible for calling Reset().
+    /// </summary>
+    [Export] private bool _autoReset;
+    /// <summary>
+    /// Recoil angle in degrees.
+    /// </summary>
+    [Export] public Vector2 _angle;
+    /// <summary>
+    /// Time to reach the recoil angle, in seconds.
+    /// </summary>
+    [Export] private float _time;
+    /// <summary>
+    /// Time to reset the recoil, in seconds.
+    /// </summary>
+    [Export] private float _resetTime;
+    /// <summary>
+    /// Indicates if recoil is only applied at the start of recoil, or if continuous recoil is also applied. <br/>
+    /// True by default.
+    /// </summary>
+    [Export] private bool _continuous = true;
     private PC_Recoil _recoilController;
 
     public override void Initialize(PC_Recoil recoilController)
     {
         _recoilController = recoilController;
     }
-    public override void Add() =>
-        AddPartial(1);
+    public override void Add()
+    {
+        if (_continuous)
+            AddPartial(1);
+    }
 
     public override void AddPartial(double subHitSize)
     {
@@ -29,7 +49,7 @@ public partial class PWR_ConstantRecoil : PW_Recoil
     public override void Start()
     {
         _recoilController.ResetBuffer();
-        Add();
+        AddPartial(1);
     }
 
     private void Complete(object sender, EventArgs e) => _recoilController.ResetRecoil(_resetTime);
