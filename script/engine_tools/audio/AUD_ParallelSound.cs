9e6fa670327a38e30cd8af4ac97bdd570c41e2d8
Leklo <leklo.sounds@gmail.com>
2025-12-29 22:00:09 +0100

    Audio overhaul - AUD_ParallelSound wip for better polyphony

--- script/engine_tools/audio/AUD_ParallelSound.cs
+++ script/engine_tools/audio/AUD_ParallelSound.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using Godot;
 using Godot.Collections;
 
@@ -6,10 +7,12 @@ public partial class AUD_ParallelSound : AUD_Sound
 {
     [Export] private AudioStreamPolyphonic _polyphonicStream; 
     [Export] private AUD_StreamPlayer _player;
-    [Export] private Array<AudioStream> _sounds = new();
+    [Export] private Array<AudioStream> _sounds = [];
     [Export] private float _minPitch = 1f;
     [Export] private float _maxPitch = 1f;
+    [Export] private float _maxPolyphony = 5f;
     private AudioStreamPlaybackPolyphonic _playback;
+    private readonly Queue<long> _voices = new();
     
     protected float _pitchBaseDelta;
     public override void _Ready()
@@ -23,7 +26,16 @@ public partial class AUD_ParallelSound : AUD_Sound
     {
         AudioStream stream = _sounds.PickRandom();
         float pitchScale = (float)GD.RandRange(_minPitch, _maxPitch) + _pitchBaseDelta;
-        _playback.PlayStream(stream, 0, 0, pitchScale);
+
+        if (_voices.Count >= _maxPolyphony)
+        {
+            long oldestVoice = _voices.Dequeue();
+            _playback.StopStream(oldestVoice);
+            _playback.SetStreamVolume(oldestVoice, -80f);
+        }
+
+        long newVoice = _playback.PlayStream(stream, 0, 0, pitchScale);
+        _voices.Enqueue(newVoice);
     }
 
     public override void Stop() =>
