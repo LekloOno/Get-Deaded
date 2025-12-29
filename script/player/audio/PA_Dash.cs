f4a268d70b67f0286d005618b1f0c760aafb3feb
Leklo <leklo.sounds@gmail.com>
2025-12-29 18:34:20 +0100

    Audio overhaul - PA_Dash adaptation

--- script/player/audio/PA_Dash.cs
+++ script/player/audio/PA_Dash.cs
@@ -1,12 +1,13 @@
 using Godot;
 
 [GlobalClass]
-public partial class PA_Dash : PA_LayeredSound
+public partial class PA_Dash : Node3D
 {
+    [Export] private AUD_Sound _sound;
     [Export] private PM_Dash _dash;
 
     public override void _Ready()
     {
-        _dash.OnStart += (o, e) => PlayLayers();
+        _dash.OnStart += (o, e) => _sound.Play();
     }
 }
