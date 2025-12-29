40749f272a64028ea22828aa656791cbe2b85296
Leklo <leklo.sounds@gmail.com>
2025-12-29 18:27:25 +0100

    Audio overhaul - PA_Land adaptation

--- script/player/audio/PA_Land.cs
+++ script/player/audio/PA_Land.cs
@@ -1,12 +1,13 @@
 using Godot;
 
 [GlobalClass]
-public partial class PA_Land : PA_LayeredSound
+public partial class PA_Land : Node3D
 {
+    [Export] private AUD_Sound _sound;
     [Export] private PS_Grounded _groundState;
 
     public override void _Ready()
     {
-        _groundState.OnLanding += (o, e) => PlayLayers();
+        _groundState.OnLanding += (o, e) => _sound.Play();
     }
 }
