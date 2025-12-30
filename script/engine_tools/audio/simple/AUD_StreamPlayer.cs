2748c76e8620203e47565bad883f50e32e0aa0eb
Leklo <leklo.sounds@gmail.com>
2025-12-30 15:52:39 +0100

    Audio overhaul - Restructured audio derivative + Layered Looper

--- script/engine_tools/audio/simple/AUD_StreamPlayer.cs
+++ script/engine_tools/audio/simple/AUD_StreamPlayer.cs
@@ -1,12 +1,10 @@
 using Godot;
 
 [GlobalClass]
-public abstract partial class AUD_StreamPlayer : Node
+public abstract partial class AUD_StreamPlayer : AUD_Sound
 {
     public abstract AudioStream Stream {get; set;}
     public abstract float VolumeDb {get; set;}
     public abstract float PitchScale {get; set;}
-    public abstract void Play();
-    public abstract void Stop();
     public abstract AudioStreamPlayback GetStreamPlayBack();
 }
