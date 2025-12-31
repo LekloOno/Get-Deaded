d09c75d8e5b94a76a05ec9368b18d2777a92c0d9
Leklo <leklo.sounds@gmail.com>
2025-12-31 17:02:03 +0100

    Audio overhaul - update AUD_ISound & AUD_Sound doc

--- script/engine_tools/audio/AUD_ISound.cs
+++ script/engine_tools/audio/AUD_ISound.cs
@@ -1,5 +1,11 @@
 public interface AUD_ISound
 {
-    public void Play();
-    public void Stop();
+    void Play();
+    void Stop();
+    float VolumeDb {get;}
+    float PitchScale {get;}
+    float BaseVolumeDb {get;}
+    float BasePitchScale {get;}
+    float RelativeVolumeDb {get; set;}
+    float RelativePitchScale {get; set;}
 }
--- script/engine_tools/audio/AUD_Sound.cs
+++ script/engine_tools/audio/AUD_Sound.cs
@@ -7,14 +7,22 @@ public abstract partial class AUD_Sound : Node, AUD_ISound
     public abstract void Stop();
     /// <summary>
     /// Retrieves the current effective VolumeDb of this sound. <br/>
-    /// Cannot be modified directly, you should either modify the RelativeVolumeDb, or BaseVolumeDb.
+    /// Cannot be modified directly, you should either modify the BaseVolumeDb in the inspector (if exposed), or RelativeVolumeDb externally.
     /// </summary>
     public abstract float VolumeDb {get; protected set;}
     /// <summary>
     /// Retrieves the current effective PitchScale of this sound.
-    /// Cannot be modified directly, you should either modify the RelativePitchScale, or BasePitchScale.
+    /// Cannot be modified directly, you should either modify the BasePitchScale in the inspector (if exposed), or RelativePitchScale externally.
     /// </summary>
     public abstract float PitchScale {get; protected set;}
+    /// <summary>
+    /// The original/initial VolumeDb of this sound. <br/>
+    /// </summary>
+    public abstract float BaseVolumeDb {get; protected set;}
+    /// <summary>
+    /// The original/initial PitchScale of this sound. <br/>
+    /// </summary>
+    public abstract float BasePitchScale {get; protected set;}
     /// <summary>
     /// Allows to modify the original VolumeDb of the sound, without overriding it. <br/>
     /// Assigning it a value of 0 will have no effect.
@@ -25,12 +33,4 @@ public abstract partial class AUD_Sound : Node, AUD_ISound
     /// Assigning it a value of 1 will have no effect.
     /// </summary>
     public abstract float RelativePitchScale {get; set;}
-    /// <summary>
-    /// The original/initial VolumeDb of this sound. <br/>
-    /// </summary>
-    public abstract float BaseVolumeDb {get; protected set;}
-    /// <summary>
-    /// The original/initial PitchScale of this sound. <br/>
-    /// </summary>
-    public abstract float BasePitchScale {get; protected set;}
 }
