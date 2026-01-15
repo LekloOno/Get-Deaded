685d58fa365de117595936b4a2ccfa9d0d6efecb
LekloOno <leklo.sounds@gmail.com>
2026-01-15 20:12:40 +0100

    Menu - Volume tab & quit button

---
+++ script/ui/menu/contexts/sound/ui_volume_slider.gd
@@ -0,0 +1,24 @@
+extends HSlider
+
+@export
+var bus_name: String
+
+var bus_index: int
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	bus_index = AudioServer.get_bus_index(bus_name)
+	value_changed.connect(_on_value_changed)
+	visibility_changed.connect(_on_visibility_changed)
+
+func _on_visibility_changed() -> void:
+	if visible:
+		value = db_to_linear(
+			AudioServer.get_bus_volume_db(bus_index)
+		)
+
+func _on_value_changed(linear_value: float) -> void:
+	AudioServer.set_bus_volume_db(
+		bus_index,
+		linear_to_db(linear_value)
+	)
