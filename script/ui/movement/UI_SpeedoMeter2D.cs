6d0abb61ed481d4dcfc5c7ae62dd8bbe03e06b17
LekloOno <leklo.sounds@gmail.com>
2026-02-24 22:54:00 +0100

    use standardmaterial options to set custom fov and anti clipping instead of double camera setup for better performances

---
+++ script/ui/movement/UI_SpeedoMeter2D.cs
@@ -0,0 +1,24 @@
+using Godot;
+
+public partial class UI_SpeedoMeter2D : Node
+{
+    [ExportCategory("Settings")]
+    [Export] private Gradient _gradient;
+    [Export] private float _minSpeed = 60f;
+    [Export] private float _maxSpeed = 10f;
+
+    [ExportCategory("Setup")]
+    [Export] private Label _label;
+    [Export] private PM_Controller _controller;
+
+    public override void _Process(double delta)
+    {
+        Vector3 faltVel = MATH_Vector3Ext.Flat(_controller.RealVelocity);
+        float speed = faltVel.Length() * 3.6f;
+        float scaledSpeed = (speed - _minSpeed)/(_maxSpeed-_minSpeed);
+        scaledSpeed = Mathf.Clamp(scaledSpeed, 0, 1);
+
+        _label.SetText((int)speed + " km/h");
+        _label.SetModulate(_gradient.Sample(scaledSpeed));
+    }
+}
