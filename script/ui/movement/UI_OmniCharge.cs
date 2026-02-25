65c87df13b227b78e7d1c7de4cac136de897b4af
LekloOno <leklo.sounds@gmail.com>
2026-02-25 18:47:50 +0100

    UI_OmniCharge - Fade in/out

--- script/ui/movement/UI_OmniCharge.cs
+++ script/ui/movement/UI_OmniCharge.cs
@@ -1,11 +1,12 @@
+using System;
 using Godot;
 
 [GlobalClass]
 public partial class UI_OmniCharge : TextureProgressBar
 {
     [Export] private PM_OmniCharge _charge;
-    [Export] private float _showTime;
-    [Export] private float _hideTime;
+    [Export] private float _showTime = 0.1f;
+    [Export] private float _hideTime = 0.5f;
     [Export] private float _blinkInTime;
     [Export] private float _blinkOutTime;
     [Export] private Color _blinkColor;
@@ -20,13 +21,34 @@ public partial class UI_OmniCharge : TextureProgressBar
     private double _targetTime;
 
     
-    private Tween _progressTween;
-    private Tween _underTween;
+    private Tween _aplhaTween;
 
     public override void _Ready()
     {
         _chargeTarget = _charge.Current / _charge.Max; 
         _chargeStart = _chargeTarget;
+
+        _charge.Consumed += Consumed;
+    }
+
+    private void Consumed(float charge)
+    {
+        _chargeTarget = _charge.Current / _charge.Max;
+        _chargeStart = _chargeTarget;
+        _aplhaTween?.Kill();
+
+        if (Modulate.A == 1f)
+            return;
+
+        SetPhysicsProcess(true);
+        SetProcess(true);
+
+        _aplhaTween = CreateTween();
+        _aplhaTween
+            .TweenProperty(this, "modulate:a", 1, _showTime)
+            .SetTrans(Tween.TransitionType.Linear)
+            .SetEase(Tween.EaseType.InOut);
+
     }
 
     public override void _PhysicsProcess(double delta)
@@ -37,10 +59,27 @@ public partial class UI_OmniCharge : TextureProgressBar
         _lerpTime = 0;
     }
 
-    public override void _Process(double delta)
+    public override async void _Process(double delta)
     {
         _lerpTime += delta;
         double lerpWeight = _lerpTime/_targetTime;
         Value = Mathf.Lerp(_chargeStart, _chargeTarget, lerpWeight);
+
+        if (Value == 1.0)
+        {
+            SetProcess(false);
+            SetPhysicsProcess(false);
+            
+            _aplhaTween?.Kill();
+
+            _aplhaTween?.Kill();
+            _aplhaTween = CreateTween();
+            _aplhaTween
+                .TweenProperty(this, "modulate:a", 0, _hideTime)
+                .SetTrans(Tween.TransitionType.Linear)
+                .SetEase(Tween.EaseType.InOut);
+
+            await ToSignal(_aplhaTween, Tween.SignalName.Finished);
+        }
     }
 }
