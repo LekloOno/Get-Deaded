bec5deb6778a6734d05150e0be31d5515ae62a9f
LekloOno <leklo.sounds@gmail.com>
2026-05-11 02:05:11 +0200

    VFX_HitscanTrail bug fix
    Trails pooling is not thread safe, there is concurrent issues.
    
    For now, i just completely removed the pooling system. A better approach would be to have one centralized request receiver that handles a single threaded pool, and receives pooling requests from concurrent requesters.

--- script/vfx/combat/trail/VFX_HitscanTrail.cs
+++ script/vfx/combat/trail/VFX_HitscanTrail.cs
@@ -1,33 +1,47 @@
-using System.Collections.Generic;
 using Godot;
 
 [GlobalClass]
 public abstract partial class VFX_HitscanTrail : VFX_Trail
 {
     [Export] protected Material _material;
-    protected Stack<VFX_ITrailMesh> _meshesPool = new();
+    //protected Stack<VFX_ITrailMesh> _meshesPool = new();
+
+    private bool _preloaded = false;
 
     public override void Preload(Node manager, uint count)
     {
+        if (_preloaded)
+            return;
+        
+        _preloaded = true;
+        VFX_TrailMesh newMesh = CreateTrail((Material)_material.Duplicate());
+        manager.AddChild(newMesh);
+        manager.RemoveChild(newMesh);
+        /*
+        if (_meshesPool.Count != 0)
+            return;
+
         for (int i = 0; i < count; i++)
         {
             VFX_TrailMesh newMesh = CreateTrail((Material)_material.Duplicate());
             newMesh.Pooled += _meshesPool.Push;
             manager.AddChild(newMesh);
             _meshesPool.Push(newMesh);
-        }
+        }*/
     }
     
     public override sealed void Shoot(Node manager, Vector3 origin, Vector3 hit)
     {
+        /*
         if (_meshesPool.TryPop(out VFX_ITrailMesh mesh))
         {
             mesh.Shoot(origin, hit);
             return;
-        }
+        }*/
 
         VFX_TrailMesh newMesh = CreateTrail((Material)_material.Duplicate());
-        newMesh.Pooled += _meshesPool.Push;
+        //newMesh.Pooled += _meshesPool.Push;
+        newMesh.Pooled += (_) => newMesh.QueueFree();
         manager.AddChild(newMesh);
         newMesh.Shoot(origin, hit);
     }
