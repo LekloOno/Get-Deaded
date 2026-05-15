e4df2357375fb4c57f556e5c9584f5c115ffe246
LekloOno <leklo.sounds@gmail.com>
2026-05-15 21:13:38 +0200

    Dirty adaptation of render scale & fsr ui code

--- script/ui/menu/contexts/video/ui_fsr_sharpness.gd
+++ script/ui/menu/contexts/video/ui_fsr_sharpness.gd
@@ -1,52 +1,20 @@
 extends Control
 
 @export var label: Control
-var slider: Slider
-
-var sharpness
 
 func _ready() -> void:
-	init_children()
-	slider.value_changed.connect(_on_value_changed)
-	visibility_changed.connect(_on_visibility_changed)
+	update_ui()
+	RenderScaleModeSetting.Changed.connect(_on_render_scale_mode_changed)
 	
-func init_children():
-	slider = get_slider()
-	
-func get_slider() -> Slider:
-	for child in get_children():
-		if child is Slider:
-			return child
-	return null
-			
-func _on_visibility_changed() -> void:
-	if !visible:
-		return
-		
-	_initialize()
-	
-	slider.value = sharpness
-	slider.value_changed.emit(sharpness)
-	
-func _initialize():
-	sharpness = get_tree().root.fsr_sharpness
-		
+func update_ui():
 	var do_show = show_scale()
 	visible = do_show
-	
 	if label:
 		label.visible = do_show
 
 func show_scale() -> bool:
-	var mode = get_tree().root.scaling_3d_mode
+	var mode = RenderScaleModeSetting.Value
 	return mode == Viewport.SCALING_3D_MODE_FSR || mode == Viewport.SCALING_3D_MODE_FSR2
-
-func _on_value_changed(new_value: float) -> void:
-	sharpness = new_value
-	get_tree().root.fsr_sharpness = sharpness
-	CONF_UserSettingsLoader.RegisterVideoSetting("fsr_sharpness", sharpness)
 	
-func _on_render_scale_mode_changed(mode: int):
-	var do_show = mode == Viewport.SCALING_3D_MODE_FSR || mode == Viewport.SCALING_3D_MODE_FSR2
-	visible = do_show
-	label.visible = do_show
+func _on_render_scale_mode_changed(sender, mode):
+	update_ui()
