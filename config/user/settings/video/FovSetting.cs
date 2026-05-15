3339c6f734ffc5fc4e0412f2f3df403d8e714f84
LekloOno <leklo.sounds@gmail.com>
2026-05-15 21:35:47 +0200

    FovSetting

---
+++ config/user/settings/video/FovSetting.cs
@@ -0,0 +1,33 @@
+using Godot;
+using TraGUS;
+
+public partial class FovSetting : UserSetting
+{
+    public override string Section => UserSettingsSection.Video;
+    public override string Key => "fov";
+
+    private PC_Settings _playerCameraSettings;
+
+    public override Variant DefaultFallBack() => 115f;
+
+    protected override bool ProcessValue(Variant value, out Variant effectiveValue)
+    {
+        if (value.VariantType != Variant.Type.Int
+        && value.VariantType != Variant.Type.Float)
+        {
+            effectiveValue = Value;
+            return false;
+        }
+
+        float fov = (float)value;
+        _playerCameraSettings.HorizontalFov = fov;
+        effectiveValue = value;
+
+        return true;
+    }
+
+    protected override void PreInitialize()
+    {
+        _playerCameraSettings = ResourceLoader.Load<PC_Settings>("res://config/player/player_camera_settings.tres");
+    }
+}
