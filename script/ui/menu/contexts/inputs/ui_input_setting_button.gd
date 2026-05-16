66e353c918aade17cb34bb59250d0411b6ae45b9
LekloOno <leklo.sounds@gmail.com>
2026-05-16 21:41:50 +0200

    Fix a few small bugs on ui_input_setting_button + cancel option

--- script/ui/menu/contexts/inputs/ui_input_setting_button.gd
+++ script/ui/menu/contexts/inputs/ui_input_setting_button.gd
@@ -27,17 +27,17 @@ func _on_bind_value_changed(sender, event: InputEvent):
 	input_event = event
 	update_ui()
 	if sender == self:
-		set_process_input(false)
-		set_pressed_no_signal(false)
+		cancel()
 		return
 	
 func update_ui():
 	if !input_event:
 		text = "n/a"
 	if input_event is InputEventMouseButton:
-		text = "mouse_" + str(input_event.button_index)
+		text = "m" + str(input_event.button_index)
 	elif input_event is InputEventKey:
-		text = OS.get_keycode_string(input_event.physical_keycode)
+		var label = DisplayServer.keyboard_get_label_from_physical(input_event.physical_keycode)
+		text = OS.get_keycode_string(label)
 
 func _on_pressed():
 	text = "listening ..."
@@ -49,10 +49,19 @@ func _on_visibility_change():
 		input_event = bind.InputEvent
 		update_ui()
 	else:
-		set_process_input(false)
-		set_pressed_no_signal(false)
+		cancel()
+		
+func cancel():
+	set_process_input(false)
+	set_pressed_no_signal(false)
 
 func _input(event: InputEvent) -> void:
+	if event.is_action("ui_cancel"):
+		get_viewport().set_input_as_handled()
+		cancel()
+		update_ui()
+		return
+	
 	if event is InputEventMouseButton:
 		if waiting_release && event.is_released() :
 			waiting_release = false
@@ -63,6 +72,7 @@ func _input(event: InputEvent) -> void:
 		send_bind(event)
 		
 func send_bind(event: InputEvent):
+	get_viewport().set_input_as_handled()
 	if input_event == event:
 		bind.TryUpdateValue(self, null)
 	elif input_event && input_event.is_match(event):
