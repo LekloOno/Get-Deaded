f31cce9905198569ef302143d6a80884babc7050
LekloOno <leklo.sounds@gmail.com>
2026-05-18 15:34:45 +0200

    Add Client API godot glue for gdscript

---
+++ Client/Api/Godot/ApiGodotGlue.cs
@@ -0,0 +1,47 @@
+using Godot;
+using Shared.Scores;
+using Client.Api.Auth;
+using Client.Api.Score;
+using System.Threading.Tasks;
+using Godot.Collections;
+
+namespace Client.Api.Godot;
+
+public partial class ApiGodotGlue : Node
+{
+    public ApiGodotGlue? Instance {get; private set;}
+    public AuthApi AuthApi {get; private set;} = new();
+    public ScoreApi ScoreApi {get; private set;} = new();
+
+    [Signal]
+    public delegate void LoginFinishedEventHandler(Dictionary result);
+
+    [Signal]
+    public delegate void RegisterFinishedEventHandler(Dictionary result);
+
+    // No signals for scores, they are a little more cumbersome to glue to gdscript
+    // since I must share the DTO. I'll keep in pure c# for now.
+    // Don't think it's worth also wrapping the DTO..
+
+    public override void _EnterTree()
+    {
+        Instance = this;
+    }
+
+    public async void Login(string username, string password)
+    {
+        AuthResult result = await AuthApi.LoginAsync(username, password);
+
+        EmitSignal(SignalName.LoginFinished, result.ToGdPayload());
+    }
+
+    public async void Register(string username, string password)
+    {
+        AuthResult result = await AuthApi.RegisterAsync(username, password);
+
+        EmitSignal(SignalName.RegisterFinished, result.ToGdPayload());
+    }
+
+    public async Task<ScoreResult> SubmitScore(SubmitScoreRequest score) =>
+        await ScoreApi.SubmitScoreAsync(score);
+}
---
+++ Client/Api/Godot/GodotResultGlue.cs
@@ -0,0 +1,29 @@
+using Client.Api.Auth;
+using Client.Api.Score;
+using Godot.Collections;
+
+namespace Client.Api.Godot;
+
+public static class ResultExtensions
+{
+    public static Dictionary ToGdPayload(this AuthResult result)
+    {
+        return new Dictionary
+        {
+            ["success"] = result.Success,
+            ["error"] = result.Error.ToString(),
+            ["message"] = result.Message ?? "",
+            ["token"] = result.Token ?? ""
+        };
+    }
+
+    public static Dictionary ToGdPayload(this ScoreResult result)
+    {
+        return new Dictionary
+        {
+            ["success"] = result.Success,
+            ["error"] = result.Error.ToString(),
+            ["message"] = result.Message ?? ""
+        };
+    }
+}
