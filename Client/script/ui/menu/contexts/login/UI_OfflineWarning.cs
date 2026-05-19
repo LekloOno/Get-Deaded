cdd0599bc5b4d7dc872edd489dda665857ce9602
LekloOno <leklo.sounds@gmail.com>
2026-05-19 07:25:34 +0200

    Missing buttons sounds

--- Client/script/ui/menu/contexts/login/UI_OfflineWarning.cs
+++ Client/script/ui/menu/contexts/login/UI_OfflineWarning.cs
@@ -4,44 +4,44 @@ using Godot;
 
 public partial class UI_OfflineWarning : Control
 {
-    [Export] private UI_EscapeMenu? _escapeMenu;
-    [Export] private Control? _difficultyMenu;
-    [Export] private Button? _offlineButton;
-    [Export] private Button? _loginButton;
-    [Export] private Button? _registerButton;
+	[Export] private UI_EscapeMenu? _escapeMenu;
+	[Export] private Control? _difficultyMenu;
+	[Export] private Button? _offlineButton;
+	[Export] private Button? _loginButton;
+	[Export] private Button? _registerButton;
 
-    [Signal]
-    public delegate void LoginEventHandler();
+	[Signal]
+	public delegate void LoginEventHandler();
 
-    [Signal]
-    public delegate void RegisterEventHandler();
+	[Signal]
+	public delegate void RegisterEventHandler();
 
-    public override void _Ready()
-    {
-        _offlineButton.Pressed += OnOfflinePressed;
-        _loginButton.Pressed += OnLoginPressed;
-        _registerButton.Pressed += OnRegisterPressed;
-    }
+	public override void _Ready()
+	{
+		_offlineButton.Pressed += OnOfflinePressed;
+		_loginButton.Pressed += OnLoginPressed;
+		_registerButton.Pressed += OnRegisterPressed;
+	}
 
-    private void OnRegisterPressed()
-    {
-        _escapeMenu.ExitCurrent();
-        EmitSignal(SignalName.Register);
-    }
+	private void OnRegisterPressed()
+	{
+		_escapeMenu.ExitCurrent();
+		EmitSignal(SignalName.Register);
+	}
 
-    private void OnLoginPressed()
-    {
-        _escapeMenu.ExitCurrent();
-        EmitSignal(SignalName.Login);
-    }
+	private void OnLoginPressed()
+	{
+		_escapeMenu.ExitCurrent();
+		EmitSignal(SignalName.Login);
+	}
 
-    private void OnOfflinePressed()
-    {
-        _escapeMenu.ExitCurrent();
-        Session.Offline = true;
-        _escapeMenu.Enter(_difficultyMenu);
-    }
+	private void OnOfflinePressed()
+	{
+		_escapeMenu.ExitCurrent();
+		Session.Offline = true;
+		_escapeMenu.Enter(_difficultyMenu);
+	}
 
-    public void EmitPlay() =>
-        EmitSignal(SignalName.Login);
+	public void EmitPlay() =>
+		EmitSignal(SignalName.Login);
 }
