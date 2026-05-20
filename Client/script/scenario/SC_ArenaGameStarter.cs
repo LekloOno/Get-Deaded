f967a60cf6a3467091dbe4bbe9af84ef46106238
LekloOno <leklo.sounds@gmail.com>
2026-05-20 16:52:09 +0200

    Add roaming mode, for testing mostly, but if players want to roam .. well they can

--- Client/script/scenario/SC_ArenaGameStarter.cs
+++ Client/script/scenario/SC_ArenaGameStarter.cs
@@ -10,6 +10,16 @@ public partial class SC_ArenaGameStarter : Node
     [Export] private SC_GameManager _gameManager;
     private PM_Controller _player;
 
+    [Signal]
+    public delegate void SceneInitializedEventHandler();
+    
+    [Signal]
+    public delegate void RoamingStartedEventHandler();
+
+    
+    [Signal]
+    public delegate void ResetedEventHandler();
+
     private bool _started;
 
     public override void _Ready()
@@ -22,32 +32,60 @@ public partial class SC_ArenaGameStarter : Node
         SC_EntitiesManager.ForceSpawn();
     }
 
-    private void Reset()
+    public void Reset()
     {
+        if (!_started)
+            return;
+
         SC_EntitiesManager.ForceSpawn();
         _menuCamera.MakeCurrent();
         Input.MouseMode = Input.MouseModeEnum.Visible;
         RemoveChild(_player);
+
         _started = false;
+        EmitSignal(SignalName.Reseted);
     }
 
     public void StartGame()
     {
-        if (_started)
+        if (!InitScene())
             return;
 
+        _gameManager.Init(_player);
+    }
+
+    public void StartRoaming()
+    {
+        if (!InitScene())
+            return;
+
+        SC_EntitiesManager.EnablePickups();
+
+        EmitSignal(SignalName.RoamingStarted);
+    }
+
+    // Returns if it did initialized or not
+    private bool InitScene()
+    {
+        if (_started)
+            return false;
+
         _started = true;
+
         SC_EntitiesManager.DisablePickups();
+
         AddChild(_player);
         _player.Revive();
         _player.GlobalPosition = _spawnPoint.GlobalPosition;
+        _player.ResetPhysicsInterpolation();
         _player.VelocityCache.DiscardCache();
         _player.Velocity = Vector3.Zero;
         _player.RealVelocity = Vector3.Zero;
         _player.Camera.MakeCurrent();
+
+        EmitSignal(SignalName.SceneInitialized);
         Input.MouseMode = Input.MouseModeEnum.Captured;
 
-        _gameManager.Init(_player);
-
+        return true;
     }
 }
