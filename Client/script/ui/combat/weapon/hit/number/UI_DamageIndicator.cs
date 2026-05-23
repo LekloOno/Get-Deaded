5d475e7fa94fb4636d152ab7053ceb347e4b10f5
LekloOno <leklo.sounds@gmail.com>
2026-05-23 20:01:33 +0200

    Enhanced damage indicators

--- Client/script/ui/combat/weapon/hit/number/UI_DamageIndicator.cs
+++ Client/script/ui/combat/weapon/hit/number/UI_DamageIndicator.cs
@@ -7,6 +7,19 @@ public partial class UI_DamageIndicator : Label
     public float HeightOffset {get; set;} = 0f;
     [Export] private float _maxHeightOffset;
     [Export] private float _fadeTime;
+    
+    [Export] private float _maxScale = 2f;
+    [Export] private float _maxScaleTime = 0.1f;
+    [Export] private Tween.TransitionType _scaleInTrans;
+    [Export] private float _minScale = 0.5f;
+    [Export] private float _normalScaleTime = 0.1f;
+    [Export] private Tween.TransitionType _scaleOutTrans;
+    [Export] private ulong _chainLifetime = 150;
+    [Export] private float _maxChainedDamage = 100f;
+    [Export] private float _minScaleDelta = 0.15f;
+    private float _chainedDamage;
+    private Tween _scaleTween;
+
     private Camera3D _camera;
     private Node3D _target;
     private float _damage;
@@ -14,30 +27,59 @@ public partial class UI_DamageIndicator : Label
     private Tween _opacityTween;
     private Tween _offsetTween;
     private Vector3 _prevTargetPosition;
+
+    private ulong _lastHit;
     
-    public void Initialize(Camera3D camera, Node3D target, float damage, Color color)
+    public void Initialize(Camera3D camera, Node3D target, float damage, Color color, bool critical)
     {
+        Scale = Vector2.Zero;
         _camera = camera;
         _target = target;
         _prevTargetPosition = target.GlobalPosition;
         _damage = damage;
         Text = (int)_damage + "";
-        AddThemeColorOverride("font_shadow_color", color);
-        StartAnim();
-    } 
 
-    public void Stack(float damage, Color color)
+        UpdateColor(color, critical);    
+        
+        StartAnim(damage);
+    }
+
+    private void UpdateColor(Color layerColor, bool critical)
+    {
+        LabelSettings.ShadowColor = layerColor;
+        
+        if (critical)
+            LabelSettings.FontColor = Colors.Orange;
+        else
+            LabelSettings.FontColor = Colors.White;
+    }
+
+    public void Stack(float damage, Color color, bool critical)
     {
         _damage += damage;
         Text = (int)_damage + "";
-        AddThemeColorOverride("font_shadow_color", color);
+        
+        UpdateColor(color, critical);
+
         _opacityTween?.Kill();
         _offsetTween?.Kill();
-        StartAnim();
+        StartAnim(damage);
     }
 
-    public void StartAnim()
+    public void StartAnim(float damage)
     {
+        DamageChainScale(damage, out float maxScale, out float minScale);
+
+        _scaleTween?.Kill();
+        _scaleTween = CreateTween();
+        _scaleTween
+            .TweenProperty(this, "scale", new Vector2(maxScale, maxScale), _maxScaleTime)
+            .SetTrans(_scaleInTrans);
+
+        _scaleTween
+            .TweenProperty(this, "scale", new Vector2(minScale, minScale), _normalScaleTime)
+            .SetTrans(_scaleOutTrans);
+
         _lastTweenTask = Anim();
     }
 
@@ -76,4 +118,25 @@ public partial class UI_DamageIndicator : Label
         Visible = !_camera.IsPositionBehind(worldPosition);
         Position = _camera.UnprojectPosition(worldPosition);
     }
+
+    private void DamageChainScale(float damage, out float peakScale, out float baseScale)
+    {
+        ulong now = PHX_Time.ScaledTicksMsec;
+        if (now - _lastHit < _chainLifetime)
+        {
+            _chainedDamage += damage;
+            peakScale = Mathf.Lerp(_maxScale, _minScale, Mathf.Min(_chainedDamage/_maxChainedDamage, 1));
+            baseScale = Mathf.Min(_minScale, peakScale - _minScaleDelta);
+            int scale = Mathf.RoundToInt(Mathf.Lerp(baseScale, peakScale, 0.5f));
+            Scale = new Vector2(scale, scale);
+        }
+        else
+        {
+            _chainedDamage = 0;
+            peakScale = _maxScale;
+            baseScale = _minScale;   
+        }
+
+        _lastHit = now;
+    } 
 }
