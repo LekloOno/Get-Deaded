f00806410d1a0cf5fc1c9e0a0d30c6706e1d3c38
LekloOno <leklo.sounds@gmail.com>
2026-05-27 18:24:29 +0200

    Dry and low ammos sounds on weapons

--- Client/script/player/audio/combat/weapons/PA_Fire.cs
+++ Client/script/player/audio/combat/weapons/PA_Fire.cs
@@ -1,14 +1,29 @@
+using System;
 using GaudioProcessTree;
 using Godot;
 
 public abstract partial class PA_Fire : Node3D
 {
     [Export] protected AUD_Sound _sound;
+    [Export] protected AUD_Sound? _lowAmmosSound;
+    [Export] protected AUD_Sound? _drySound;
+    [Export] private float _lowAmmosStartPitch = 1f;
+    [Export] private float _lowAmmosEndPitch = 0.7f;
+    [Export] private float _lowAmmosStartVolume = -20f;
+    [Export] private float _lowAmmosEndVolume = 0f;
     public abstract PW_Fire Fire {get;}
+
+    const uint MagPickRatio = 20;
 
     public override void _Ready()
     {
         Fire.Shot += ShotSound;
+        Fire.DryShot += DrySound;
+    }
+
+    private void DrySound()
+    {
+        _drySound?.Play();
     }
 
     /// <summary>
@@ -16,6 +31,46 @@ public abstract partial class PA_Fire : Node3D
     /// </summary>
     /// <param name="sender"></param>
     /// <param name="shots"></param>
-    public virtual void ShotSound(object sender, int shots) =>
+    public virtual void ShotSound(object sender, int shots)
+    {
         _sound.Play();
+
+        if (_lowAmmosSound == null)
+            return;
+
+        uint magPick = Fire.Ammos.MagazinePick;
+        if (magPick == 0)
+            return;
+
+        uint loaded = Fire.Ammos.LoadedAmmos;
+
+        uint minLoaded = (magPick * 10 + MagPickRatio - 1) / MagPickRatio;
+         
+        if (loaded <= minLoaded)
+            PlayLowAmmos(_lowAmmosSound, loaded, minLoaded);
+    }
+
+    private void PlayLowAmmos(AUD_Sound sound, uint loaded, uint minLoaded)
+    {
+        float ratio = (float)loaded/minLoaded;
+
+        float pitch = Mathf.Lerp
+        (
+            _lowAmmosEndPitch,
+            _lowAmmosStartPitch,
+            ratio
+        );
+        
+        float volumeDb = Mathf.Lerp
+        (
+            _lowAmmosEndVolume,
+            _lowAmmosStartVolume,
+            ratio
+        );
+
+        sound.RelativeVolumeDb = volumeDb;
+        sound.RelativePitchScale = pitch;
+        sound.Play();
+    }
+
 }
