4c2f8310ff6f0763beec44be6a2ac36ee932eaeb
LekloOno <leklo.sounds@gmail.com>
2026-05-27 20:35:43 +0200

    Use animations on LowLoaded ui

--- Client/script/ui/combat/weapon/loadout/ammo/UIW_LowLoaded.cs
+++ Client/script/ui/combat/weapon/loadout/ammo/UIW_LowLoaded.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Threading.Tasks;
 using Godot;
 
 [GlobalClass]
@@ -6,15 +8,28 @@ public partial class UIW_LowLoaded : Control
     [Export] private Label? _label;
     // Later use a custom displayable texture rect probably.
     [Export] private Label? _key;
+    
+    [Export] private ANIM_InOutTweenSetting? _opacityTweenSettings;
+    private Tween? _opacityTween;
+    [Export] private ANIM_InOutTweenSetting? _scaleTweenSettings;
+    [Export] private Vector2 _startScale;
+    private Tween? _scaleTween;
+
     private KeyBindingSetting? _bind;
+
+    private bool _shown;
 
     public override void _Ready()
     {
         _bind = KeyBindingSettingsManager.Instance.GetBinding(ACTIONS_Combat.RELOAD);
         
         OnBindChanged(this, 0);
-        
+
         _bind.Changed += OnBindChanged;
+
+        Color mod = Modulate;
+        mod.A = 0f;
+        Modulate = mod;
     }
 
     private void OnBindChanged(GodotObject sender, Variant value)
@@ -40,5 +55,43 @@ public partial class UIW_LowLoaded : Control
             _key.Text = OS.GetKeycodeString(
                 DisplayServer.KeyboardGetLabelFromPhysical(keyButton.PhysicalKeycode)
             );
+    }
+
+    public void StartShow()
+    {
+        if (_shown)
+            return;
+
+        _shown = true;
+
+        Color mod = Modulate;
+        mod.A = 0f;
+        Modulate = mod;
+
+        Scale = _startScale;
+        
+        _opacityTween?.Kill();
+        _opacityTween = CreateTween();
+        _opacityTweenSettings?.FadeIn?.TweenProperty(_opacityTween, this);
+
+        _scaleTween?.Kill();
+        _scaleTween = CreateTween();
+        _scaleTweenSettings?.FadeIn?.TweenProperty(_scaleTween, this);
+    }
+
+    public void StartHide()
+    {
+        if (!_shown)
+            return;
+
+        _shown = false;
+
+        _opacityTween?.Kill();
+        _opacityTween = CreateTween();
+        _opacityTweenSettings?.FadeOut?.TweenProperty(_opacityTween, this);
+
+        _scaleTween?.Kill();
+        _scaleTween = CreateTween();
+        _scaleTweenSettings?.FadeIn?.TweenProperty(_scaleTween, this);
     }
 }
