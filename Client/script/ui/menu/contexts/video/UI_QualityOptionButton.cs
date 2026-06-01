ca291248106540950965051f611e79f3c2b850db
LekloOno <leklo.sounds@gmail.com>
2026-06-02 00:51:29 +0200

    UI_QualityOptionButton - generic option button for video quality settings

---
+++ Client/script/ui/menu/contexts/video/UI_QualityOptionButton.cs
@@ -0,0 +1,97 @@
+using System;
+using Godot;
+using TraGUS;
+
+[GlobalClass]
+public partial class UI_QualityOptionButton : OptionButton
+{
+    [Export(PropertyHint.Flags)]
+    private VideoQualityMask _allowedQualities =
+        VideoQualityMask.Minimal |
+        VideoQualityMask.Low |
+        VideoQualityMask.Medium |
+        VideoQualityMask.High |
+        VideoQualityMask.Ultra;
+
+    [Export]
+    private string? _settingKey;
+
+    private UserSetting? _userSetting;
+
+    public override void _Ready()
+    {
+        if (_settingKey == null)
+        {
+            GD.PrintErr("[UI_QualityOptionButton] Missing _userSetting.");
+            return;
+        }
+
+        UserSettingsServer.GetSetting("video", _settingKey, out _userSetting);
+        
+        if (_userSetting == null)
+        {
+            GD.PrintErr($"[UI_QualityOptionButton] UserSettingsServer : '{_userSetting}' is not a valid registered setting key.");
+            return;
+        }
+
+        Populate();
+
+        SetQuality((VideoQualitySetting) (int) _userSetting.Value);
+        _userSetting.Changed += OnSettingChanged;
+        
+        ItemSelected += OnItemSelected;
+    }
+
+    private void OnSettingChanged(GodotObject sender, Variant value)
+    {
+        if (sender == this)
+            return;
+
+        SetQuality((VideoQualitySetting) (int) value);
+    }
+
+    private void Populate()
+    {
+        Clear();
+
+        foreach (VideoQualitySetting quality in Enum.GetValues<VideoQualitySetting>())
+        {
+            var mask = quality.ToMask();
+
+            if (!_allowedQualities.HasFlag(mask))
+                continue;
+
+            int index = ItemCount;
+
+            AddItem($"{quality.ToString().ToUpperInvariant()}_QUALITY_OPTION");
+            SetItemMetadata(index, (int)quality);
+        }
+    }
+
+    private void SetQuality(VideoQualitySetting quality)
+    {
+        for (int i = 0; i < ItemCount; i++)
+        {
+            var itemQuality = (VideoQualitySetting)(int)GetItemMetadata(i);
+
+            if (itemQuality == quality)
+            {
+                Select(i);
+                return;
+            }
+        }
+
+        GD.PushWarning($"[UI_QualityOptionButton] Quality {quality} is not available in this OptionButton.");
+    }
+
+    private void OnItemSelected(long index)
+    {
+        if (_userSetting == null)
+            return;
+
+        var quality = (VideoQualitySetting)(int)GetItemMetadata((int)index);
+        
+        if (!_userSetting.TryUpdateValue(this, (int) quality, out Variant effectiveValue))
+            SetQuality((VideoQualitySetting) (int) effectiveValue);
+    }
+}
