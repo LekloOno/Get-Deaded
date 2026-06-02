243aa1927ae720d7b57dfd0527a3899dc4fa0a7f
LekloOno <leklo.sounds@gmail.com>
2026-06-02 18:43:52 +0200

    SsilSetting

---
+++ Client/config/user/settings/video/quality/environment/SsilSetting.cs
@@ -0,0 +1,71 @@
+using Godot;
+
+public partial class SsilSetting : VideoQualitySetting
+{
+    public override string Key => "dynamic_indirect_lighting";
+
+    protected override void UpdateFrom(VideoQuality quality)
+    {
+        Environment? env = GetViewport()?.World3D?.Environment;
+
+        if (env == null)
+        {
+            GD.PrintErr("[ScreenSpaceIndirectLighting] no Environment found.");
+            return;
+        }
+
+        UpdateEnvironment(env);
+    }
+    
+    private static SsilQuality From(VideoQuality quality)
+    {
+        return quality switch
+        {
+            VideoQuality.Minimal => Minimal,
+            VideoQuality.Low => Low,
+            VideoQuality.Medium => Medium,
+            VideoQuality.High => High,
+            VideoQuality.Ultra => Ultra,
+            _ => Minimal,
+        };
+    }
+
+    public static void UpdateEnvironment(Environment env)
+    {        
+        if (Quality == VideoQuality.Disabled)
+        {
+            env.SsilEnabled = false;
+            return;
+        }
+
+        env.SsilEnabled = true;
+
+        SsilQuality settings = From(Quality);
+        
+        RenderingServer.EnvironmentSetSsilQuality(
+            settings.Quality,
+            true,
+            1f,
+            4,
+            50f,
+            300f);
+    }
+
+    private readonly static SsilQuality Minimal = new(
+        RenderingServer.EnvironmentSsilQuality.VeryLow);
+
+    private readonly static SsilQuality Low = new(
+        RenderingServer.EnvironmentSsilQuality.Low);
+
+    private readonly static SsilQuality Medium = new(
+        RenderingServer.EnvironmentSsilQuality.Medium);
+
+    private readonly static SsilQuality High = new(
+        RenderingServer.EnvironmentSsilQuality.High);
+
+    private readonly static SsilQuality Ultra = new(
+        RenderingServer.EnvironmentSsilQuality.Ultra);
+}
+
+public record SsilQuality(
+    RenderingServer.EnvironmentSsilQuality Quality);
