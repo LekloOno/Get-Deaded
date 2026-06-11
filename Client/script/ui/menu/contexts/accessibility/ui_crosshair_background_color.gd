8bb5f72d96713b31ed3fb5413277d688673b360d
LekloOno <leklo.sounds@gmail.com>
2026-06-11 23:18:23 +0200

    Use editor crosshair background color in browser

--- Client/script/ui/menu/contexts/accessibility/ui_crosshair_background_color.gd
+++ Client/script/ui/menu/contexts/accessibility/ui_crosshair_background_color.gd
@@ -1,27 +1,14 @@
 extends ColorPickerButton
 
-@export var crosshair_background: PanelContainer
-
-var _panel_stylebox: StyleBoxFlat
-
+@export var crosshair_background: StyleBoxFlat
 
 func _ready() -> void:
 	if not crosshair_background:
 		return
 
-	var stylebox := crosshair_background.get_theme_stylebox("panel")
-
-	if stylebox is StyleBoxFlat:
-		_panel_stylebox = stylebox.duplicate()
-	else:
-		_panel_stylebox = StyleBoxFlat.new()
-
-	crosshair_background.add_theme_stylebox_override("panel", _panel_stylebox)
-
-	color = _panel_stylebox.bg_color
+	color = crosshair_background.bg_color
 	color_changed.connect(_on_color_changed)
 
-
 func _on_color_changed(new_color: Color) -> void:
-	if _panel_stylebox:
-		_panel_stylebox.bg_color = new_color
+	if crosshair_background:
+		crosshair_background.bg_color = new_color
