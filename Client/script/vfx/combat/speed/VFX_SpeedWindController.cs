ce9a2f6c09e68d9e50b2e622d6317ddc8c1235cd
LekloOno <leklo.sounds@gmail.com>
2026-06-12 17:16:11 +0200

    ui_speed_vfx shader manual phase option + VFX_SpeedWindLayer & Controller

---
+++ Client/script/vfx/combat/speed/VFX_SpeedWindController.cs
@@ -0,0 +1,39 @@
+using System.Collections.Generic;
+using Godot;
+
+[Tool]
+public partial class VFX_SpeedWindController : Control
+{
+    [Export(PropertyHint.Range, "0,1")]
+    public float Intensity;
+
+    private readonly List<VFX_SpeedWindLayer> _layers = [];
+
+    public override void _EnterTree()
+    {
+        SetChildren();
+        SetProcess(true);
+    }
+
+    public override void _Process(double delta)
+    {
+        foreach (VFX_SpeedWindLayer layer in _layers)
+            layer.Intensity = Intensity;
+    }
+
+    public override void _Notification(int what)
+    {
+        if (what != NotificationChildOrderChanged)
+            return;
+        
+        SetChildren();
+    }
+
+    private void SetChildren()
+    {
+        _layers.Clear();
+        foreach(Node node in GetChildren())
+            if (node is VFX_SpeedWindLayer layer)
+                _layers.Add(layer);
+    }
+}
---
+++ Client/script/vfx/combat/speed/VFX_SpeedWindLayer.cs
@@ -0,0 +1,30 @@
+using Godot;
+
+[Tool]
+public partial class VFX_SpeedWindLayer : Control
+{
+    [Export] private float _speed = 4f;
+    [Export] private float _frequency = 0.1f;
+
+    [Export(PropertyHint.Range, "0,1")]
+    public  float Intensity;
+    
+    private double _phase = 0.0;
+
+    public override void _EnterTree()
+    {
+        SetProcess(true);
+    }
+
+    public override void _Process(double delta)
+    {
+        float effectiveSpeed = Mathf.Lerp(0.2f, 1.0f, Intensity) * _speed;
+        float effectiveFrequency = Mathf.Lerp(0.1f, 1.0f, Intensity) * _frequency;
+
+        _phase += effectiveSpeed * effectiveFrequency * delta;
+
+        SetInstanceShaderParameter("intensity", Intensity);
+        SetInstanceShaderParameter("phase", (float)_phase);
+        SetInstanceShaderParameter("manual_frequency", effectiveFrequency);
+    }
+}
