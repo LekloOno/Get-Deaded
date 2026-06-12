4835952268d274f7d3189810f2524ba8bb718e97
LekloOno <leklo.sounds@gmail.com>
2026-06-12 20:56:04 +0200

    Make dash/double jump sound generic

--- Client/script/player/audio/PA_InstantAction.cs
+++ Client/script/player/audio/PA_InstantAction.cs
@@ -4,8 +4,8 @@ using Godot;
 [GlobalClass]
 public partial class PA_InstantAction : Node3D
 {
-    [Export] private AUD_Sound _sound;
-    [Export] private PM_Action _action;
+    [Export] private AUD_Sound _sound = null!;
+    [Export] private PM_Action _action = null!;
 
     public override void _Ready() =>
         _action.Started += _sound.Play;
