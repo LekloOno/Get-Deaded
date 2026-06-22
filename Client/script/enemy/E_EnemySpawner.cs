e88c64b3baad26dad9e986fd0e9079bfb5423f08
LekloOno <leklo.sounds@gmail.com>
2026-06-23 00:36:41 +0200

    E_EnemySpawner - make it pool based

--- Client/script/enemy/E_EnemySpawner.cs
+++ Client/script/enemy/E_EnemySpawner.cs
@@ -1,30 +1,63 @@
+using System;
+using System.Collections.Generic;
 using Godot;
 
 [GlobalClass]
-public partial class E_EnemySpawner : Node3D, E_IEnemyComponent
+public partial class E_EnemySpawner : Node3D
 {
-    public E_IEnemy? Enemy {get; set;}
-    private NodePath _enemyPath = null!;
-    [Export] public NodePath EnemyPath
-    {
-        get => _enemyPath;
-        set => this.SetEnemy(this, ref _enemyPath, value);
-    }
+    [Export] private PackedScene _enemy = null!;
 
     [Export] private float _respawnDelay;
     private SceneTreeTimer? _respawnTimer;
     private SceneTreeTimer? _hideTimer;
 
+    private readonly Stack<E_IEnemy> _pool = [];
+
+    const uint Prealloc = 3;
 
     public override void _Ready()
     {
-        (this as E_IEnemyComponent).ResolveEnemy(this);
+        InitEnemies();
+        Respawn();
     }
+
+    private void InitEnemies()
+    {
+        for (int i = 0; i < Prealloc; i ++)
+        {
+            E_IEnemy enemy = CreateEnemy();
+
+            if (enemy is not Node node)
+                return;
+
+            AddChild(node);
+
+            enemy.Spawn();
+            enemy.Pool();
+            _pool.Push(enemy);
+        }
+    }
+
+    private E_IEnemy CreateEnemy()
+    {
+        E_IEnemy enemy = _enemy.Instantiate<E_IEnemy>();
+
+        enemy.Died += OnDied;
+        enemy.Disabled += OnDisabled;
+
+        return enemy;
+    }
+
 
     public void Respawn()
     {
-        Enemy!.Body.Teleport(GlobalPosition);
-        Enemy.Spawn();
+        E_IEnemy enemy;
+
+        if (!_pool.TryPop(out enemy!))
+            enemy = CreateEnemy();
+
+        enemy.Body.Teleport(GlobalPosition);
+        enemy.Spawn();
     }
 
     public void OnDied(E_IEnemy enemy, GC_Health senderLayer)
@@ -32,8 +65,8 @@ public partial class E_EnemySpawner : Node3D, E_IEnemyComponent
         _respawnTimer = GetTree().CreateTimer(_respawnDelay, false, true);
         _respawnTimer.Timeout += Respawn;
     }
-    public void OnPooled(E_IEnemy enemy) {}
-    public void OnSpawned() {}
-    public void OnDisabled(E_IEnemy enemy) {}
-    public void OnEnemyChanged(E_IEnemy? prev, E_IEnemy? next) {}
+    public void OnDisabled(E_IEnemy enemy)
+    {
+        _pool.Push(enemy);
+    }
 }
