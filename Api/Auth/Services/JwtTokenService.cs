4f4e7deb3e0eed95d42dc6d8edffc1c39f46eff1
LekloOno <leklo.sounds@gmail.com>
2026-07-11 23:25:59 +0200

    Api/Auth - make JwtTokenService settings cleaner

--- Api/Auth/Services/JwtTokenService.cs
+++ Api/Auth/Services/JwtTokenService.cs
@@ -2,25 +2,24 @@ using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
 using System.Text;
 using Data.Entities;
+using Microsoft.Extensions.Options;
 using Microsoft.IdentityModel.Tokens;
 
 namespace Api.Auth.Services;
 
 public class JwtTokenService : IJwtTokenService
 {
-    private readonly IConfiguration _config;
+    private readonly JwtSettings _settings;
+    private readonly SymmetricSecurityKey _signingKey;
 
-    public JwtTokenService(IConfiguration config)
+    public JwtTokenService(IOptions<JwtSettings> options)
     {
-        _config = config;
+        _settings = options.Value;
+        _signingKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_settings.Key));
     }
 
     public string CreateToken(Player player)
     {
-        var key = _config["Jwt:Key"]!;
-        var issuer = _config["Jwt:Issuer"]!;
-        var audience = _config["Jwt:Audience"]!;
-
         var claims = new List<Claim>
         {
             new Claim(JwtRegisteredClaimNames.Sub, player.Id.ToString()),
@@ -28,15 +27,11 @@ public class JwtTokenService : IJwtTokenService
             new Claim(ClaimTypes.Name, player.Username)
         };
 
-        var signingKey = new SymmetricSecurityKey(
-            Encoding.UTF8.GetBytes(key)
-        );
-
-        var creds = new SigningCredentials(signingKey, SecurityAlgorithms.HmacSha256);
+        var creds = new SigningCredentials(_signingKey, SecurityAlgorithms.HmacSha256);
 
         var token = new JwtSecurityToken(
-            issuer: issuer,
-            audience: audience,
+            issuer: _settings.Issuer,
+            audience: _settings.Audience,
             claims: claims,
             expires: DateTime.UtcNow.AddDays(7),
             signingCredentials: creds
